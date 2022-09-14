93fd4ac907136ceb7d81211bff0a154452a7fe74
nicholas <nja8161995@gmail.com>
2022-09-14 16:25:47 -0500

    Created query selectors and eventlistener

---
+++ script.js
@@ -0,0 +1,20 @@
+const submitBtn = document.querySelector("button");
+const fName = document.querySelector("#firstName");
+const lName = document.querySelector("#lastName");
+const userEmail = document.querySelector("#email");
+const userPhone = document.querySelector("#phone");
+const userPassword = document.querySelector("#password");
+const userConfirmed = document.querySelector("#confirmPassword");
+
+function isValid() {
+
+}
+
+function confirmPassword() {
+
+}
+
+submitBtn.addEventListener("click", () => {
+  isValid();
+  confirmPassword();
+});
