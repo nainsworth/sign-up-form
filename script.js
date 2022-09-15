24f205606a6001bbeb3f9c2a2dc9477dac61cd2e
nicholas <nja8161995@gmail.com>
2022-09-15 16:21:13 -0500

    Added a feature to display errors and unmatched passwords

--- script.js
+++ script.js
@@ -5,19 +5,50 @@ const userEmail = document.querySelector("#email");
 const userPhone = document.querySelector("#phone");
 const userPassword = document.querySelector("#password");
 const userConfirmed = document.querySelector("#confirmPassword");
-let error = false
+const errorMsg = document.querySelector(".error-msg");
 
-function isValid() {
+const inputs = [
+  fName,
+  lName,
+  userEmail,
+  userPhone,
+  userPassword,
+  userConfirmed,
+];
 
-}
+// Removes error class when focused on
+inputs.forEach((input) => {
+  input.addEventListener("keydown", () => {
+    errorMsg.textContent = "";
+    input.classList.remove("error");
+    //Clears errors in both password and confirmed password inputs
+    if (input === userPassword || input === userConfirmed) {
+      userPassword.classList.remove("error");
+      userConfirmed.classList.remove("error");
+    }
+  });
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("invalid", () => {
+    input.classList.add("error");
+    errorMsg.textContent = "* Some fields were entered incorrectly";
+  });
+});
 
 function confirmPassword(pwd, confirmed) {
-  if (pwd !== confirmed) {
-    userPassword.classList.add(".error");
-    
+  if (pwd.value !== confirmed.value) {
+    userPassword.classList.add("error");
+    userConfirmed.classList.add("error");
+    userPassword.value = "";
+    userConfirmed.value = "";
+    errorMsg.textContent = "* Passwords don't match";
+    return false;
   }
 }
 
-submitBtn.addEventListener("click", () => {
-  confirmPassword(userPassword, userConfirmed);
+submitBtn.addEventListener("click", (e) => {
+  if (confirmPassword(userPassword, userConfirmed) === false) {
+    e.preventDefault();
+  }
 });
